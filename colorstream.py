b197ca3b9927cb99981a41ad9ebe79fe2b69615b
Jan Dohl <jan@saphira.polynet>
2009-08-14 14:56:35 +0200

    Finished basic stuffs

---
+++ colorstream.py
@@ -0,0 +1,35 @@
+from utils import speedcalc
+from utils import asciicolor
+
+class ColorStream:
+  def __init__(self):
+    self.time = 0
+    self.commands = {}
+    self.lastcolor = (0,0,0)
+
+  def wait(self, time):
+    self.time += time
+
+  def setColor(self, color):
+    self.commands[self.time] = ('set', color)
+    self.time += 10
+
+  def fadeColor(self, color, time):
+    speed = speedcalc(self.lastcolor, color, time)
+    if speed > 1000:
+      print('Warning, resulting speed is bigger than 1000')
+      speed = 1000
+    self.commands[self.time] = ('fade', color, speed)
+    self.time += time
+
+  def createCommands(self, id):
+    cmds = {}
+    for e in self.commands:
+      time = e
+      cmd = self.commands[time]
+      if cmd[0] == 'set':
+        cstring = 'C ' + id + ' ' + asciicolor(cmd[1])
+      else:
+        cstring = 'F ' + id + ' ' + asciicolor(cmd[1]) + ' ' + str(cmd[2])
+      cmds[time] = cstring
+    return cmds
---
+++ utils.py
@@ -0,0 +1,44 @@
+def interweave(a, b, maxdelay=100):
+  r = {}
+  a_keys = a.keys()
+  a_keys.sort()
+  b_keys = b.keys()
+  b_keys.sort()
+
+  while (len(a_keys) > 0) or (len(b_keys) > 0):
+    if (len(a_keys) > 0) and (len(b_keys) > 0):
+      if a_keys[0] < b_keys[0]:
+        time = a_keys.pop(0)
+        value = a[time]
+      else:
+        time = b_keys.pop(0)
+        value = b[time]
+    elif len(a_keys) > 0:
+      time = a_keys.pop(0)
+      value = a[time]
+    else:
+      time = b_keys.pop(0)
+      value = b[time]
+
+    otime = time
+    while r.keys().count(time) > 0:
+      time = time + 10
+      if time - otime > maxdelay:
+        raise RuntimeError('Timeindex had to be moved further than maxdelay')
+
+    r[time] = value
+
+  return r
+
+def speedcalc(color1, color2, time):
+  factor = 1777.647 # The magic Speed-Time translation factor
+  length = max(abs(color1[0]-color2[0]), abs(color1[1]-color2[1]), abs(color1[2]-color2[2]))
+  speed = int((length * factor) / time)
+  return speed
+
+def asciicolor(color):
+  colstr = '#'
+  for c in color:
+    colstr += hex(c / 16)[2]
+    colstr += hex(c % 16)[2]
+  return colstr
